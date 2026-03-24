0af2ab8748b3dd925a79989043140396a25c3932
Romain-Chevrollier <romain2002.chevrollier@gmail.com>
2026-03-24 16:19:11 +0100

    feat: dockerize FastAPI app with joblib model loading

--- src/model.py
+++ src/model.py
@@ -4,16 +4,11 @@ import numpy as np
 import joblib
 
 # Configurer le tracking URI (même chose que dans ton notebook)
-mlflow.set_tracking_uri("sqlite:///" + os.path.abspath("mlflow.db"))
+# mlflow.set_tracking_uri("sqlite:///" + os.path.abspath("mlflow.db"))
 
 def load_model():
-    try:
-        model = mlflow.pyfunc.load_model("models:/sleep-model-lgbm@champion")
-        return model
-    except Exception as e:
-        print(f"Erreur chargement modèle : {e}")
-        raise
-    mlflow.pyfunc.load_model("models:/sleep-model-lgbm@champion")
+    model = joblib.load("data/best_model.joblib")
+    return model
 
 
 def load_scaler():
